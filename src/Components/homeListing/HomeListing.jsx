58b020e23b36c6b956315c4914393b977fdc8a19
Mostafa Khidr <mostafa5idr.ace@gmail.com>
2023-11-18 19:33:35 +0200

    update the home data --mostafaACE

--- src/Components/homeListing/HomeListing.jsx
+++ src/Components/homeListing/HomeListing.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ProductCardCols from "../productCardCols/ProductCardCols";
 import { productsCol } from "../../config/firebase/firebase";
 import { useCollectionData } from "react-firebase-hooks/firestore";
@@ -5,13 +6,19 @@ import styles from "./homeListing.module.css";
 
 const HomeListing = () => {
   const [products] = useCollectionData(productsCol);
-  const displayedProducts = products?.slice(0, 8).map((product) => {
+
+  const filteredProducts = products
+    ? products.filter((product) => !product.userDescription)
+    : [];
+
+  const displayedProducts = filteredProducts.slice(0, 8).map((product) => {
     return <ProductCardCols product={product} key={product.id} />;
   });
+
   return (
     <section className={styles.homeListing_container}>
       <h2>blabla</h2>
-      <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {displayedProducts}
       </div>
     </section>
